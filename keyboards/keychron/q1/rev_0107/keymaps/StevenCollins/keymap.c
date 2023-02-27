a8ea94d9eb32769216ea331f015afdef3efa2d2f
Steven Collins <StevenCollins@users.noreply.github.com>
2023-02-27 17:41:44 -0500

    Knob default control swaps for Mac/PC

--- keyboards/keychron/q1/rev_0107/keymaps/StevenCollins/keymap.c
+++ keyboards/keychron/q1/rev_0107/keymaps/StevenCollins/keymap.c
@@ -196,24 +196,24 @@ bool dip_switch_update_user(uint8_t index, bool active) {
     return true;
 }
 
-// Send left/right or volume up/down from knob
+// Send volume up/down or left/right from knob
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        switch (get_highest_layer(layer_state)) {
+        switch (get_highest_layer(layer_state | default_layer_state)) {
             case MAC_BASE:
-            case WIN_BASE:
-                    if (clockwise) {
-                        tap_code_delay(KC_RIGHT, 2);
-                    } else {
-                        tap_code_delay(KC_LEFT, 2);
-                    }
-                break;
-            case MAC_FN:
             case WIN_FN:
                 if (clockwise) {
                     tap_code_delay(KC_VOLU, 2);
                 } else {
                     tap_code_delay(KC_VOLD, 2);
+                }
+                break;
+            case MAC_FN:
+            case WIN_BASE:
+                if (clockwise) {
+                    tap_code_delay(KC_RIGHT, 2);
+                } else {
+                    tap_code_delay(KC_LEFT, 2);
                 }
                 break;
         }
